26c8e08a071c250cc0629c4976d2cf8c9a14004f
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2022-05-20 12:12:13 +0200

    Create get_server_check view.

--- transactions/views.py
+++ transactions/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
 
 # Create your views here.
+@api_view(['GET'])
+def get_server_check(request):
+    server_check_message = {'message': 'Server is ready for work'}
+    return Response(server_check_message)
