c57cd2522a1c113d5d1776da6278184de21a86f3
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2022-05-20 15:33:44 +0200

    Create base test for the /report to check if json is in response.

--- transactions/tests.py
+++ transactions/tests.py
@@ -1,8 +1,16 @@
 # from django.test import TestCase
 from django.urls import reverse
 from rest_framework import status
-from rest_framework.test import APITestCase, URLPatternsTestCase
+from rest_framework.test import APITestCase
+import json
 
+
+def validate_json(json_data):
+    try:
+        json.loads(json_data)
+    except ValueError as err:
+        return False
+    return True
 
 # Create your tests here.
 
@@ -19,3 +27,30 @@ class ReportTests(APITestCase):
         # then
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['message'], 'Server is ready for work.')
+
+    def test_simplified_pay_by_link_report(self):
+        """
+        Ensure server is ready to work.
+        """
+        # given
+        url = reverse('report')
+        data = {
+            "pay_by_link": [
+                {
+                    "created_at": "2021-05-13T01:01:43-08:00",
+                    "currency": "EUR",
+                    "amount": "3000",
+                    "description": "Gym membership",
+                    "bank": "mbank"
+                }
+            ]
+        }
+
+        # when
+        response = self.client.post(url, data, format='application/json')
+
+        # then
+        response_first_obj = response.data[0]
+        is_valid_jason = validate_json(response_first_obj)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue(is_valid_jason)
