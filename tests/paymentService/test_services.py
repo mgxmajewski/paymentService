c91444b572d5bd7bda7bbe99af64fc93e662af74
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2022-05-22 22:00:11 +0200

    Parametrize test_process_transaction.

--- tests/paymentService/test_services.py
+++ tests/paymentService/test_services.py
@@ -1,6 +1,8 @@
 import pytest
 from assertpy import assert_that
-from transactions.services import create_payment_info, pay_by_link_payment_info, TransactionToProcess
+
+from transactions.services import create_payment_info, pay_by_link_payment_info, dp_payment_info, card_payment_info, \
+    TransactionToProcess
 
 
 @pytest.mark.django_db
@@ -10,17 +12,32 @@ class TestTransactionsServices:
     def prepare_get_payment_info(self):
         self.process_transaction = create_payment_info
 
-    def test_process_transaction(self):
+    # case1
+    processing_strategy_1 = pay_by_link_payment_info
+    expected_1 = 'pay_by_link'
+    case_1 = processing_strategy_1, expected_1
+
+    # case2
+    processing_strategy_2 = dp_payment_info
+    expected_2 = 'dp'
+    case_2 = processing_strategy_2, expected_2
+
+    # case3
+    processing_strategy_3 = card_payment_info
+    expected_3 = 'card'
+    case_3 = processing_strategy_3, expected_3
+
+    @pytest.mark.parametrize("processing_strategy, expected", [case_1, case_2, case_3])
+    def test_process_transaction(self, processing_strategy, expected):
         """
         Ensure transaction type after processing matches the chosen strategy
         """
 
         # given
-        processing_strategy = pay_by_link_payment_info
         data = TransactionToProcess()
 
         # when
         result = self.process_transaction(processing_strategy, data).type
 
         # then
-        assert_that(result).is_equal_to('pay_by_link')
+        assert_that(result).is_equal_to(expected)
--- transactions/services.py
+++ transactions/services.py
@@ -16,6 +16,16 @@ def pay_by_link_payment_info(data: TransactionToProcess):
     return PaymentInfo('pay_by_link')
 
 
+def dp_payment_info(data: TransactionToProcess):
+    print(data)
+    return
+
+
+def card_payment_info(data: TransactionToProcess):
+    print(data)
+    return
+
+
 def create_payment_info(processing_strategy_fn, data: TransactionToProcess):
     payment_info = processing_strategy_fn(data)
     return payment_info
