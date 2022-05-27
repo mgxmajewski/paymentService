10acca42eb15059867bfb0d9dbd7de07a961a689
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2022-05-27 23:58:59 +0200

    Create save_report view.

--- transactions/views.py
+++ transactions/views.py
@@ -1,6 +1,7 @@
 from rest_framework import status
+from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, renderer_classes
 from .services import process_request, InvalidDateString
 
 
@@ -12,7 +13,18 @@ def get_server_check(request):
 
 
 @api_view(['POST'])
+# @renderer_classes([JSONRenderer])
 def generate_report(request):
+    try:
+        report = process_request(request.data)
+    except InvalidDateString:
+        return Response('Bad request: "created_at" string(date-field) not valid.', status=status.HTTP_400_BAD_REQUEST)
+    return Response(report, content_type='application/json')
+
+
+@api_view(['POST'])
+# @renderer_classes([JSONRenderer])
+def save_report(request):
     try:
         report = process_request(request.data)
     except InvalidDateString:
