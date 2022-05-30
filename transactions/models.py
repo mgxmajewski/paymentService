f6155b93ebf6bf7dcff61725bbc347ffadcbf09c
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2022-05-30 13:46:37 +0200

    Adjust PaymentInfoModel and PaymentInfoModelSerializer.

--- transactions/models.py
+++ transactions/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 
 class PaymentInfoModel(models.Model):
-    date: models.CharField(max_length=30)
-    type: models.CharField(max_length=30)
-    description: models.CharField(max_length=30)
-    currency: models.CharField(max_length=30)
-    payment_mean: models.CharField(max_length=30)
-    amount: models.IntegerField()
-    amount_in_pln: models.IntegerField()
+    date = models.CharField(max_length=40)
+    type = models.CharField(max_length=30)
+    description = models.CharField(max_length=30)
+    currency = models.CharField(max_length=30)
+    payment_mean = models.CharField(max_length=30)
+    amount = models.IntegerField()
+    amount_in_pln = models.IntegerField()
--- transactions/serializers.py
+++ transactions/serializers.py
@@ -2,24 +2,8 @@ from rest_framework import serializers
 from .models import PaymentInfoModel
 
 
-class PaymentInfoModelSerializer(serializers.ModelSerializer):
+class PaymentInfoSerializer(serializers.ModelSerializer):
     class Meta:
         model = PaymentInfoModel
         fields = ['date', 'type', 'payment_mean', 'description', 'currency', 'amount', 'amount_in_pln']
 
-
-class PaymentInfoSerializer(serializers.Serializer):
-    id: serializers.IntegerField(read_only=True)
-    date: serializers.CharField(max_length=30)
-    type: serializers.CharField(max_length=30)
-    amount: serializers.IntegerField()
-    description: serializers.CharField(max_length=30)
-    currency: serializers.CharField(max_length=30)
-    payment_mean: serializers.CharField(max_length=30)
-    amount_in_pln: serializers.IntegerField()
-
-    def create(self, validated_data):
-        """
-        Create and return a new `Snippet` instance, given the validated data.
-        """
-        return PaymentInfoModel.objects.create(**validated_data)
