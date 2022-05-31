532b8b36565aba10de583fd1bde2bd27087e7ee7
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2022-05-31 23:29:27 +0200

    Add ReportModel.

--- transactions/models.py
+++ transactions/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 
 
+class ReportModel(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.IntegerField()
+
+
 class PaymentInfoModel(models.Model):
+    report = models.ForeignKey('ReportModel', on_delete=models.CASCADE)
     date = models.CharField(max_length=40)
     type = models.CharField(max_length=30)
     description = models.CharField(max_length=30)
