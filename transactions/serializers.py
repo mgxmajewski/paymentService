e2dcea40c6b4dec110a561e6deeb706dd6c69873
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2022-05-31 23:29:48 +0200

    Add ReportSerializer.

--- transactions/serializers.py
+++ transactions/serializers.py
@@ -1,9 +1,14 @@
 from rest_framework import serializers
-from .models import PaymentInfoModel
+from .models import PaymentInfoModel, ReportModel
+
+
+class ReportSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ReportModel
+        fields = ['user']
 
 
 class PaymentInfoSerializer(serializers.ModelSerializer):
     class Meta:
         model = PaymentInfoModel
-        fields = ['date', 'type', 'payment_mean', 'description', 'currency', 'amount', 'amount_in_pln']
-
+        fields = ['report', 'date', 'type', 'payment_mean', 'description', 'currency', 'amount', 'amount_in_pln']
