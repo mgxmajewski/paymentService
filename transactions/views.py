ea2335ae2bdcdbdcdd829898d0495c0508e9aa4d
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2022-05-31 23:30:13 +0200

    Change save_report view.

--- transactions/views.py
+++ transactions/views.py
@@ -2,6 +2,8 @@ from rest_framework import status
 from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, renderer_classes
+
+from .serializers import PaymentInfoSerializer, ReportSerializer
 from .services import process_request, InvalidDateString
 
 
@@ -25,9 +27,19 @@ def generate_report(request):
 @api_view(['POST'])
 # @renderer_classes([JSONRenderer])
 def save_report(request, pk):
+    report_serializer = ReportSerializer(data={'user': pk})
+    if report_serializer.is_valid():
+        report_serializer.save()
     try:
-        report = process_request(request.data)
-        print(pk)
+        data = process_request(request.data)
+        for payment in data:
+            print(payment)
+            # payment[]
+            print(report_serializer.data)
+            payment_serializer = PaymentInfoSerializer(data=payment, context={'report_id': 1})
+            print(payment_serializer)
+            if payment_serializer.is_valid():
+                payment_serializer.save(report_id=report_serializer)
     except InvalidDateString:
         return Response('Bad request: "created_at" string(date-field) not valid.', status=status.HTTP_400_BAD_REQUEST)
-    return Response(report, content_type='application/json')
+    return Response(data, content_type='application/json')
